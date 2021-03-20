a9db93c796c69953c274d82bd9adeca3e19f15e1
Ayman <aymanbourramouss@gmail.com>
2021-03-20 14:18:06 +0100

    cooking elements part 1
    Added a zone that detects when an element is put inside

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import Phaser, { Game } from 'phaser';
+import Phaser, { Game, GameObjects } from 'phaser';
 import deskImg from './assets/Screenshot_3.jpg';
 import bun from './assets/bread1.png'
 import sausage from './assets/sausage1.png'
@@ -18,10 +18,10 @@ class MyGame extends Phaser.Scene
     preload ()
     {
         this.load.image('desk', deskImg);
-        this.load.image('bun', bun);
-        this.load.image('sausage', sausage);
         this.load.image('ketchup', ketchup);
         this.load.image('ketchupFill', ketchupFill);
+        this.load.image('bun', bun);
+        this.load.image('sausage', sausage);
 
         //this.load.image('bunSal', 'assets/bread2.png');
         //this.load.image('bunSalKet', 'assets/bread3.png');
@@ -30,12 +30,15 @@ class MyGame extends Phaser.Scene
       
     create ()
     {
+
         const y = 160
-        var panCounter = 0;
-        var sausageCounter = 0;
+        let panCounter = 0;
+        let sausageCounter = 0;
         let posPan = [new Point(210,y),new Point(260,y),new Point(310,y)]
         let posSausage = [new Point(460,y),new Point(510,y),new Point(560,y)]
-           
+        let spriteBum = []
+        let spriteSaus = []
+        let insideZone = []
 
         function changeTexture() {
             this.setTexture('ketchupFill');
@@ -75,34 +78,75 @@ class MyGame extends Phaser.Scene
         
         //mice position
         this.button = this.add.sprite(300, 200,'desk').setInteractive();
-
-        //ketchup-bottle
-        var ket = createEntity(this,'ketchup',40,60);
-        ket.on('pointerdown',changeTexture).on('pointerup',unchange);
-
-        
-        
-        
         //debug
         this.button.on('pointerdown',function(){
             console.log("x: "+  game.input.mousePointer.x + "/" + "y: "+game.input.mousePointer.y);
         },this)
         
-        //pan
-        var pan = collider(this,'bun', 150,340,220,270,posPan,panCounter);
-        //Salchicha
-        var salchicha = collider(this,'sausage', 400,600,220,270,posSausage,sausageCounter);
+        
+        //GAME ELEMENTS:
+        //cooking plate
+        var zone = this.add.zone(510,170, 250, 110).setRectangleDropZone(250, 110);
 
+        //  Just a visual display of the drop zone
+        var graphics = this.add.graphics();
+        graphics.lineStyle(2, 0xffff00);
+        graphics.strokeRect(zone.x - zone.input.hitArea.width / 2, zone.y - zone.input.hitArea.height / 2, zone.input.hitArea.width, zone.input.hitArea.height);
+
+        //pan
+        collider(this,'bun', 150,340,220,270,posPan,panCounter);
+        //Salchicha
+        collider(this,'sausage', 400,600,220,270,posSausage,sausageCounter);
+
+        
+        //ketchup-bottle
+        let ket = createEntity(this,'ketchup',40,60);
+        ket.on('pointerdown',changeTexture).on('pointerup',unchange);
+
+      
 
         this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
         gameObject.x = dragX;
         gameObject.y = dragY;
         });
         
+    
+        this.input.on('dragover', function (pointer, gameObject, dropZone) {
+            if(!insideZone.includes(gameObject) ){
+                insideZone.push(gameObject);
+                console.log("count: " + insideZone.length);
+                graphics.clear();
+                graphics.lineStyle(2, 0x00ffff);
+                graphics.strokeRect(zone.x - zone.input.hitArea.width / 2, zone.y - zone.input.hitArea.height / 2, zone.input.hitArea.width, zone.input.hitArea.height);
+            }
+        });
+    
+        this.input.on('dragleave', function (pointer, gameObject, dropZone) {
+            
+            let i = insideZone.find(gameObject);
+            console.log(i);
+            
+            if(insideZone.length != 0){
+                console.log("count: " + insideZone.length);
+            }
+            else{
+                console.log("count: " + insideZone.length);
+                graphics.clear();
+                graphics.lineStyle(2, 0xffff00);
+                graphics.strokeRect(zone.x - zone.input.hitArea.width / 2, zone.y - zone.input.hitArea.height / 2, zone.input.hitArea.width, zone.input.hitArea.height);
+            }
+        });
+
+
+
+
 
     }
 
-   
+    update(){
+        
+    }
+    
 }
  
 
